379b1b68b6dcadd5364b2410826a833b50948332
48thFlame <79379047+48thFlame@users.noreply.github.com>
2025-10-05 19:01:17 +0300

    basic counter

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func newModel() model {
+	return model{count: 0}
+}
+
+type model struct {
+	count int
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "a":
+			m.count--
+
+		case "s":
+			m.count++
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	return strconv.Itoa(m.count)
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	p := tea.NewProgram(newModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
